7d5ae451b55b78c89e7c0bf51960b677e18a1097
FairyWind <53415234+FairyWind@users.noreply.github.com>
2019-12-20 10:59:16 +0800

    JSAAL-17 Add investments value card (#4)
    * JSAAL-17 investments value card backend
    
    * JSAAL-17 change user_id to userId
    
    * JSAAL-17 delete middleware

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const investments = require('./controller/investments');
 
 const { PORT } = process.env;
 const app = express();
 
 app.use(express.json());
+app.use('/investments', investments);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-  console.log('the app is running');
+  console.log(`the app is running at port ${PORT}`);
 });
 
 module.exports = app;
---
+++ src/controller/investments.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const userInvestments = require('../data/data');
+
+const investments = express.Router();
+
+investments.get('/user/:uid', (req, res) => {
+  const { uid } = req.params;
+  const userData = userInvestments.find((data) => data.userId === uid);
+  if (userData) {
+    return res.send(userData);
+  }
+  return res.sendStatus(404);
+});
+
+module.exports = investments;
