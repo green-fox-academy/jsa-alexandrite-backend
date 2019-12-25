966b259852bc1fe3661691802b68ed71515ef920
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-25 17:27:42 +0800

    JSAAL-73 Adding initial schemas (#6)
    * JSAAL-73 Adding initial schemas
    
    * integrating passport
    
    * Added missing files and passport
    
    * removing emp line
    
    * adding enums for transaction type and status

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ const passport = require('passport');
 const { db, User } = require('./models');
 const { users, investments } = require('./controller');
 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('connected to database');
+});
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 const { PORT } = process.env;
 
 db.on('error', console.error.bind(console, 'connection error:'));
