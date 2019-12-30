f5d668817a878e264139a378d99952662c1fa439
Jiacheng Jiang <ajax15601560911@gmail.com>
2019-12-30 12:23:17 +0800

    basic wl restore

--- src/controller/watchlists.js
+++ src/controller/watchlists.js
@@ -7,9 +7,22 @@ watchlists.post('/backup', async (req, res) => {
   const id = '5e01dc0fd399fd162f3e5788';
   try {
     const user = await User.findById(id);
+    if (!Array.isArray(req.body)) return res.sendStatus(400);
+    if (!user) return res.sendStatus(404);
     user.watchlists = req.body;
     const result = await user.save();
-    return res.send(result);
+    return res.send(result.watchlists);
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+});
+
+watchlists.get('/restore', async (req, res) => {
+  const id = '5e01dc0fd399fd162f3e5788';
+  try {
+    const user = await User.findById(id);
+    if (!user) return res.sendStatus(400);
+    return res.send(user.watchlists);
   } catch (err) {
     return res.sendStatus(500);
   }
