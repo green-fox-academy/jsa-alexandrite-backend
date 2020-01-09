72ed21924215fbf4c6b4e1b6a806eb0e09fda330
xiaoxing <775132026@qq.com>
2020-01-09 18:12:45 +0800

    JSAAL-65 finished order backend

--- src/controller/order.js
+++ src/controller/order.js
@@ -6,20 +6,20 @@ const Transaction = require('../models/transaction');
 const router = Router();
 
 const secret = process.env.ACCESS_TOKEN_SECRET;
+const { API_URL, API_KEY } = process.env;
 
 const order = async (req, res) => {
   try {
-    const { API_URL, API_KEY } = process.env;
     const {
-      shares, price, symbol, type,
+      shares, symbol, type,
     } = req.body;
     const url = new URL(`${API_URL}/stock/${symbol}/batch`);
     url.searchParams.append('types', 'quote,company');
     url.searchParams.append('token', API_KEY);
-    const result = fetch(url);
-    console.log(result);
+    const data = await (await fetch(url)).json();
     const { id } = req.user;
     const status = 'failed';
+    const price = data.quote.latestPrice;
     const orderTransaction = new Transaction(
       {
         user: id, shares, price, symbol, type, status,
