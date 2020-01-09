372c3604fa526e192eb006ae9c739a2524727169
xiaoxing <775132026@qq.com>
2020-01-09 23:20:01 +0800

    JSAAL-65 add order

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,17 +1,11 @@
 const users = require('./users');
 const investments = require('./investments');
-<<<<<<< HEAD
 const account = require('./account');
-=======
 const order = require('./order');
->>>>>>> JSAAL-65 basically finished order backend
 
 module.exports = {
   users,
   investments,
-<<<<<<< HEAD
   account,
-=======
   order,
->>>>>>> JSAAL-65 basically finished order backend
 };
