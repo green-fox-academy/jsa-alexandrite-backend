e672cc756ec4ea7cad5318f2d5e565cccc17ec1d
FairyWind <53415234+FairyWind@users.noreply.github.com>
2020-01-14 13:06:48 +0800

    JSAAL-63 top-up money backend (#13)
    * JSAAL-63 top-up money backend
    
    * JSAAL-63 can save to transaction
    
    * JSAAL-63 fix response
    
    * JSAAL-63 fix problems
    
    * JSAAL-63 fix problems
    
    * JSAAL-63 remove one try-catch

--- src/controller/account.js
+++ src/controller/account.js
@@ -4,13 +4,36 @@ const jwtVerifier = require('express-jwt');
 const secret = process.env.ACCESS_TOKEN_SECRET;
 
 const account = express.Router();
-const { User } = require('../models');
+const { User, Transaction } = require('../models');
 
 account.get('/user', jwtVerifier({ secret }), async (req, res) => {
   try {
     const { id } = req.user;
     const user = await User.find({ _id: id }, '-_id username balance');
     return res.send(user);
+  } catch (err) {
+    return res.sendStatus(500);
+  }
+});
+
+account.post('/topup', jwtVerifier({ secret }), async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { amount } = req.body;
+    if (amount < 0) {
+      return res.status(400).send('Sorry, top-up amount should be over 0');
+    }
+    const { balance } = await User.findById(id, 'balance');
+    const result = balance + amount;
+    await User.updateOne({ _id: id }, { $set: { balance: result } });
+    const topUpTransaction = new Transaction({
+      user: id,
+      amount,
+      type: 'topUp',
+      status: 'settled',
+    });
+    await topUpTransaction.save();
+    return res.json({ balance: result });
   } catch (err) {
     return res.sendStatus(500);
   }
