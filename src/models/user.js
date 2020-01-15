9060aa9f0269afb55762af1dd9749b9b73592b1b
Jiacheng Jiang <ajax15601560911@gmail.com>
2020-01-15 16:31:02 +0800

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -2,18 +2,21 @@ const { Schema, model } = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
 const { watchlistSchema } = require('./watchlist');
 
+
+const investmentSchema = new Schema({
+  symbol: String,
+  shares: Number,
+  sector: String,
+  entryPrice: Number,
+});
+
 const userSchema = new Schema({
   username: { type: String, required: true },
   balance: {
     type: Number,
     default: 0,
   },
-  investments: [{
-    symbol: String,
-    shares: Number,
-    sector: String,
-    entryPrice: Number,
-  }],
+  investments: [investmentSchema],
   watchlists: [watchlistSchema],
 });
 
